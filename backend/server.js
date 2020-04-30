445cdfa7575565a40bedf2b8a464b5d29717df4e
João Costa <joaocosta@MacBook-Pro-de-Joao.local>
2020-04-30 23:18:25 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const mongoose = require('mongoose');
+require('dotenv').config()
+//bring routes
+const blogRoutes = require('./routes/blog')
+const authRoutes = require('./routes/auth')
+const userRoutes = require('./routes/user')
+const categoryRoutes = require('./routes/category')
+const tagRoutes = require('./routes/tag')
+const formRoutes = require('./routes/form')
+// app
+
+
+const app = express()
+
+// middlewares
+app.use(morgan('dev'))
+app.use(bodyParser.json())
+app.use(cookieParser())
+// cors
+if(process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+
+// db
+mongoose
+  .connect(process.env.DATABASE, { useNewUrlParser: true, useCreateIndex:true, useFindAndModifiy: false })
+  .then(() => console.log('DB Connected'));
+
+// routes middleware
+app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', categoryRoutes);
+app.use('/api', tagRoutes);
+app.use('/api', formRoutes);
+
+
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+})
---
+++ frontend/components/Header.js
@@ -0,0 +1,108 @@
+import { useState } from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import { APP_NAME } from '../config';
+import { signout, isAuth } from '../actions/auth';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+import Search from './blog/Search';
+
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
+
+const Header = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => {
+    setCollapsed(!collapsed);
+  };
+
+  return (
+    <React.Fragment>
+      <Navbar color="light" light >
+        <Link href="/">
+          <NavLink className="font-weight-bold">{APP_NAME}</NavLink>
+        </Link>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2"/>
+        <Collapse isOpen={!collapsed} navbar>         
+          <Nav className="ml-auto" navbar>      
+            <Search />   
+            <React.Fragment>
+            <NavItem>
+                <Link href="/blogs">
+                  <NavLink>Blogs</NavLink>
+                </Link>
+              </NavItem>
+
+              <NavItem>
+                <Link href="/contact">
+                  <NavLink>Contact</NavLink>
+                </Link>
+              </NavItem>
+            </React.Fragment>
+
+            {!isAuth() && (
+              <React.Fragment>
+                <NavItem>
+                  <Link href="/signin">
+                    <NavLink>Signin</NavLink>
+                  </Link>
+                </NavItem>
+                <NavItem>
+                  <Link href="/signup">
+                    <NavLink>Signup</NavLink>
+                  </Link>
+                </NavItem>
+              </React.Fragment>
+            )}
+
+            {isAuth() && isAuth().role === 0 && (
+              <NavItem>
+                <Link href="/user">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+
+            {isAuth() && isAuth().role === 1 && (
+              <NavItem>
+                <Link href="/admin">
+                  <NavLink>{`${isAuth().name}'s Dashboard`}</NavLink>
+                </Link>
+              </NavItem>
+            )}
+
+            {isAuth() && (
+              <NavItem>
+                <NavLink style={{ cursor: 'pointer' }} onClick={() => signout(() => Router.replace(`/signin`))}>
+                  Signout
+                </NavLink>
+              </NavItem>
+            )}
+
+            <NavItem>
+              <Link href="/user/crud/blog">
+                <NavLink className="btn btn-primary text-light">Write a blog</NavLink>
+              </Link>
+            </NavItem>              
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </React.Fragment>
+  );
+};
+
+export default Header;
